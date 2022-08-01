35178d9c654776a290e2ea33e029fe719e0e2a2c
Szoke Balazs <balazs.szoke@360.world>
2022-08-01 23:21:41 +0200

    Initial command line tool

---
+++ Pragmatic-runner.py
@@ -0,0 +1,11 @@
+# -*- coding: ascii -*-
+
+
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+
+from Pragmatic.Pragmatic import Main
+
+
+if __name__ == '__main__':
+	Main()
