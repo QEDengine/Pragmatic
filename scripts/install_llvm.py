0c0c4df97cf827f317542025b80121e984f416e6
Szoke Balazs <balazs.szoke@360.world>
2022-10-30 23:31:14 +0100

    LLVM CI build

---
+++ scripts/install_llvm.py
@@ -0,0 +1,15 @@
+
+from os import path
+import subprocess
+import pathlib
+import os
+import shutil
+
+if not pathlib.Path('Install').exists():
+	os.makedirs('Install')
+
+cmake_install_command = 'cmake --install . --config MinSizeRel --prefix "./../install"'
+subprocess.run(f'cd Build && {cmake_install_command}', shell=True)
+
+shutil.copyfile('Build\MinSizeRel\lib\clang.lib', 'Install\lib\clang.lib')
+shutil.copyfile('Build\MinSizeRel\lib\clang.exp', 'Install\lib\clang.exp')
